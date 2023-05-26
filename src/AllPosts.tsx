8c1106d35b5ce81c79a8cc0cebb565a4af7d7a73
Zachary Piontek <96935362+Zachary-Piontek@users.noreply.github.com>
2023-05-26 15:36:37 -0700

    added files for components need for build

---
+++ src/AllPosts.tsx
@@ -0,0 +1,7 @@
+export default function AllPosts() {
+  return (
+    <div>
+      <h1>All Posts</h1>
+    </div>
+  );
+}
---
+++ src/MessageBoard.tsx
@@ -0,0 +1,7 @@
+export default function MessageBoard() {
+  return (
+    <div>
+      <h1>Message Board</h1>
+    </div>
+  );
+}
