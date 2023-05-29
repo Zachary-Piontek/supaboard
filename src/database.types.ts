b4e60938197fce9d8f1f793fdff79b3d13e49b02
Zachary Piontek <96935362+Zachary-Piontek@users.noreply.github.com>
2023-05-29 16:59:17 -0700

    added create post and data types

--- src/database.types.ts
+++ src/database.types.ts
@@ -4,8 +4,27 @@ export type Json =
   | boolean
   | null
   | { [key: string]: Json }
-  | Json[]
+  | Json[];
 
+  export interface GetPostsResponse {
+    created_at: string;
+    id: string;
+    score: number;
+    title: string;
+    user_id: string;
+    username: string;
+  }
+  
+  export interface GetSinglePostWithCommentResponse {
+    author_name: string;
+    content: string;
+    created_at: string;
+    id: string;
+    path: string;
+    score: number;
+    title: string;
+  }
+  
 export interface Database {
   graphql_public: {
     Tables: {
